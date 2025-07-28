7aeedfc7e357640c46f58277c26b9c7894964c7f
Setu Jha <setukrjha9040@gmail.com>
2025-07-29 00:31:16 +0530

    updated redis url connection

--- clash/server/src/config/queue.ts
+++ clash/server/src/config/queue.ts
@@ -1,8 +1,7 @@
 import { ConnectionOptions, DefaultJobOptions } from "bullmq"; // Importing ConnectionOptions from bullmq ,used for configuring the connection to the Redis server
 
 export const redisConnection: ConnectionOptions = { // Configuration for connecting to Redis server 
-    host: process.env.REDIS_HOST, // Hostname of the Redis server
-    port: 6379,
+    url: process.env.REDIS_URL, // URL of the Redis server
 }
 
 export const defaultQueueOptions: DefaultJobOptions = { // Default options for jobs in the queue to how long a job can run before it is considered failed,or we need to hold it in the queue 
